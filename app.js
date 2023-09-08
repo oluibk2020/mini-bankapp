8c02ab01ea2ddd18e261bc987c6d7919cec57ae1
Olugbenga Taiwo <taiwoolugbenga2018@gmail.com>
2023-09-08 18:37:10 +0100

    edited the msg error

--- app.js
+++ app.js
@@ -1,108 +1,105 @@
 const add = document.querySelector("#add");
-const screen = document.querySelector("#screen")
+const screen = document.querySelector("#screen");
 const withdraw = document.querySelector("#withdraw");
 const transaction = document.querySelector("#history");
 const form = document.querySelector("form");
-const msg = document.querySelector('#message')
+const msg = document.querySelector("#message");
 
+form.addEventListener("submit", (e) => {
+  let date = new Date();
+  date = `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  console.log(date);
+  e.preventDefault();
+  const formAmount = document.querySelector("#amount").value;
+  wallet.deposit(Number(formAmount), date);
+});
 
-form.addEventListener('submit', (e) => {
-    let date = new Date()
-    date = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-    console.log(date);
-    e.preventDefault()
-    const formAmount = document.querySelector("#amount").value;
-    wallet.deposit(Number(formAmount), date)
-    
-})
+withdraw.addEventListener("click", (e) => {
+  let date = new Date();
+  date = `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  console.log(date);
+  const formAmount = document.querySelector("#amount").value;
 
-withdraw.addEventListener('click', (e) => {
-    let date = new Date()
-    date = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-    console.log(date);
-    const formAmount = document.querySelector("#amount").value;
+  e.preventDefault();
+  wallet.withdraw(Number(formAmount), date);
+});
 
-    e.preventDefault()
-    wallet.withdraw(Number(formAmount), date)
-})
-
-transaction.addEventListener('click', (e) => {
-    e.preventDefault()
-   const lists = wallet.getTransactions()
-   console.log(lists);
-   let li = ''
-   for (const list of lists) {
-       li += `<li>The transaction type is ${list.type}, the amount is $${list.amount} and transaction date is ${list.date}</li>`
-    document.querySelector('ol').innerHTML = li
-   }
-})
-
-
+transaction.addEventListener("click", (e) => {
+  e.preventDefault();
+  const lists = wallet.getTransactions();
+  console.log(lists);
+  let li = "";
+  for (const list of lists) {
+    li += `<li>The transaction type is ${list.type}, the amount is $${list.amount} and transaction date is ${list.date}</li>`;
+    document.querySelector("ol").innerHTML = li;
+  }
+});
 
 class Wallet {
-    constructor() {
-        this._balance = 0;
-        this._transactions = [];
-        this._transactionDate = ''
-    }
-    
-    deposit(amount, dateTrans) {
-        this._balance += amount;
-        this._transactionDate = dateTrans
-        this._processDeposit(amount, dateTrans);
-        this._updateScreen()
-        msg.textContent = `$${amount} has been withdrawn successfully`
-               msg.style.color = 'green'
-        this._clearInput()
-    }
-    
-    withdraw(amount, dateTrans) {
-        if (amount > this._balance || amount === 0) {
-               msg.textContent = "Not enough funds"
-               msg.style.color = 'red'
-               this._clearInput()
-           return
-        } else {
-            this._balance -= amount;
-            this._transactionDate = dateTrans
-            this._processWithdraw(amount, dateTrans);
-            this._updateScreen()
-            msg.textContent = `$${amount} has been withdrawn successfully`
-               msg.style.color = 'blue'
-            this._clearInput()
-            return
-        }
-    }
-    
-    _processDeposit(amount,date) {
-    this._transactions.push({ type: "deposit", amount,date });
-}
-_processWithdraw(amount,date) {
-    this._transactions.push({ type: "withdraw", amount , date});
-}
+  constructor() {
+    this._balance = 0;
+    this._transactions = [];
+    this._transactionDate = "";
+  }
 
-_updateScreen(){
-    screen.innerText = this.getBalance()
-}
+  deposit(amount, dateTrans) {
+    this._balance += amount;
+    this._transactionDate = dateTrans;
+    this._processDeposit(amount, dateTrans);
+    this._updateScreen();
+    msg.textContent = `$${amount} has been added successfully`;
+    msg.style.color = "green";
+    this._clearInput();
+  }
 
-_clearInput(){
-    document.querySelector("#amount").value = 0
+  withdraw(amount, dateTrans) {
+    if (amount > this._balance || amount === 0) {
+      msg.textContent = "Not enough funds";
+      msg.style.color = "red";
+      this._clearInput();
+      return;
+    } else {
+      this._balance -= amount;
+      this._transactionDate = dateTrans;
+      this._processWithdraw(amount, dateTrans);
+      this._updateScreen();
+      msg.textContent = `$${amount} has been withdrawn successfully`;
+      msg.style.color = "blue";
+      this._clearInput();
+      return;
+    }
+  }
+
+  _processDeposit(amount, date) {
+    this._transactions.push({ type: "deposit", amount, date });
+  }
+  _processWithdraw(amount, date) {
+    this._transactions.push({ type: "withdraw", amount, date });
+  }
+
+  _updateScreen() {
+    screen.innerText = this.getBalance();
+  }
+
+  _clearInput() {
+    document.querySelector("#amount").value = 0;
     setTimeout(() => {
-        document.querySelector('#message').textContent = ''
+      document.querySelector("#message").textContent = "";
     }, 3000);
-}
+  }
 
-getBalance() {
+  getBalance() {
     return this._balance;
-}
+  }
 
-
-getTransactions() {
+  getTransactions() {
     return this._transactions;
+  }
 }
-
-}
-
 
 // console.log(amount);
 
